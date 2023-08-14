34b52ece3627e8a31b7a784c422d5ee38840f652
SerhiiZakorko <129493695+SerhiiZakorko@users.noreply.github.com>
2023-08-14 22:11:31 +0200

    Add files via upload

--- HW.sql
+++ HW.sql
@@ -1,15 +1,47 @@
--- Вывести данные о стримах (проекция: названиестрима, имяавтора_стрима)
+-- 1. Вывести ко-во поставщиков не из UK и не из China
 
-SELECT streams.title, users.username
-FROM streams
-left JOIN users ON streams.author_id = users.user_id
-
-
-
--- Вывести юзеров, у которых отсутствуют стримы.
+SELECT COUNT(*) 
+FROM Suppliers 
+WHERE Country NOT IN ('UK', 'China');
 
 
-SELECT users.*
-FROM users
-LEFT JOIN streams ON users.user_id = streams.author_id
-WHERE users.created_at IS NULL;
+-- 2. Вывести среднюю/MAX/MIN стоимости и ко-во товаров из категорий 3 и 5
+
+SELECT 
+COUNT(*) AS total_products,
+AVG(Price) AS avg_price,
+MAX(Price) AS max_price,
+MIN(Price) AS min_price
+FROM Products
+where
+ProductID IN (3, 5)
+
+-- 3. Вывести общую сумму проданных товаров
+
+SELECT 
+  sum(t2.price * t1.quantity)
+FROM orderdetails t1
+INNER JOIN products t2
+on t1.productID = t2.productID
+
+-- 4. Вывести ко-во стран, которые поставляют напитки
+
+SELECT 
+   COUNT(DISTINCT suppliers.Country) AS count_countries
+FROM products
+
+JOIN suppliers ON products.SupplierID=suppliers.SupplierID
+JOIN categories ON products.CategoryID=categories.CategoryID
+
+WHERE
+   categories.CategoryName='Beverages';
+
+-- 5. Вывести сумму, на которую было отправлено товаров клиентам в USA
+
+SELECT SUM(Products.Price * OrderDetails.Quantity) AS order_cost
+FROM Customers
+join Orders on Customers.CustomerID=Orders.CustomerID
+join OrderDetails on Orders.OrderID=OrderDetails.OrderID
+join Products on OrderDetails.ProductID=Products.ProductID
+where
+Customers.Country = "USA"
